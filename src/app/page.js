e43b4570966dad87f280f24c970576a5b3205d02
atylicki9 <78560574+atylicki9@users.noreply.github.com>
2023-08-29 12:55:54 -0400

    add states to ui and front end logic

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,31 @@
-import Image from 'next/image'
+'use client';
+
 import styles from './page.module.css'
+import { useState } from 'react'
 
 export default function Home() {
+
+  const [codingLanguage, setCodingLanguage] = useState("C#");
+  const [prompt, setPrompt] = useState("");
+  const [codeOutput, setCodeOutput] = useState("Code will appear here!");
+
+  function appendAdditionalPromptData(promptFromInput) {
+    // add additional parameters to prompt like length, coding lang, etc.
+    return `${promptFromInput} in ${codingLanguage}. Make the response code only and have no comments.`
+  }
+
+  function submitCodeSnippetRequest(promptInput) {
+    const fullPrompt = appendAdditionalPromptData(promptInput);
+    console.log(`Prompt: ${fullPrompt}`)
+    
+    // send prompt to openAI
+    const codeSnippetResponse = `Request sent with prompt ${fullPrompt} Connect to chat gpt later for this to work.`;
+    console.log(`Response: ${codeSnippetResponse}`)
+
+    // show response in codeOutput
+    setCodeOutput(codeSnippetResponse);
+  }
+
   return (
     <main className={styles.main}>
       <div className={styles.title}>
@@ -11,19 +35,24 @@ export default function Home() {
       </div>
       <div className={styles.center} >
         <div className={styles.io}>
-            <input className={styles.prompt}/>
-            <select className={styles.selectDropdown}>
+            <input 
+              placeholder='Try something like "Write me a function for adding two numbers together"' 
+              className={styles.prompt}
+              value={prompt}
+              onChange={(e) => setPrompt(e.target.value)}
+            />
+            <select className={styles.selectDropdown} value={codingLanguage} onChange={(e) => setCodingLanguage(e.target.value)}>
               <option value="C#">C#</option>
               <option value="JavaScript">JavaScript</option>
               <option value="Python">Python</option>
               <option value="TypeScript">TypeScript</option>
             </select>
-            <button className={styles.submitButton}>Submit</button>
+            <button className={styles.submitButton} onClick={e => submitCodeSnippetRequest(prompt)}>Submit</button>
         </div>
         <div className={styles.io}>
           <div className={styles.output}>
             <p>
-              <code className={styles.code}>Enter a prompt above, youre a code snippet will appear here! </code>
+              <code className={styles.code}>{codeOutput}</code>
             </p>
           </div>
         </div>
